28f953d8d4f2a1bd3f981757550ae5ab667a3ea9
Allen Guo <guosonglin001@gmail.com>
2025-02-28 00:48:23 +0800

    wip

--- transform/linalg.matmul.0.sh
+++ transform/linalg.matmul.0.sh
@@ -63,3 +63,42 @@ args=(
 mlir-opt "${args[@]}"
 
 ###############################################################################
+
+PIPELINE=""
+PIPELINE+="transform-interpreter{entry-point=__transform_main},"
+PIPELINE+="lower-affine,"
+PIPELINE+="convert-scf-to-cf,"
+PIPELINE+="func.func(convert-arith-to-llvm),"
+# TODO: update when pr merge
+# https://github.com/llvm/llvm-project/pull/128219
+PIPELINE+="convert-vector-to-llvm{vector-contract-lowering=matmul vector-transpose-lowering=flat},"
+PIPELINE+="convert-to-llvm,"
+PIPELINE+="canonicalize"
+PIPELINE="builtin.module("$PIPELINE")"
+DIR=_demos/tmp
+mkdir -p $DIR
+args=(
+  --pass-pipeline="$PIPELINE"
+  --dump-pass-pipeline
+  -o $DIR/mlir-opt.mlir
+  transform/linalg.matmul.0.mlir
+)
+mlir-opt "${args[@]}"
+
+######
+
+# TODO: add a pass
+# rm transform-dialect manually
+# get `main.mlir`
+
+######
+
+DIR=_demos/tmp
+args=(
+  --mlir-to-llvmir
+  -o $DIR/main.ll
+  $DIR/main.mlir
+)
+mlir-translate "${args[@]}"
+
+###############################################################################
