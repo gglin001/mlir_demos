9ea0eb034cb62660cefbdf83da255dfe0cf64dec
Allen Guo <guosonglin001@gmail.com>
2025-02-28 15:53:46 +0800

    misc

--- misc/lit.cfg.py
+++ misc/lit.cfg.py
@@ -9,6 +9,6 @@ config: lit.TestingConfig.TestingConfig
 
 config.name = "mlir_demos"
 config.test_format = lit.formats.ShTest(execute_external=True)
-config.suffixes = [".mlir"]
+config.suffixes = set([".mlir"])
 
 config.test_exec_root = os.path.join(tempfile.gettempdir(), "lit")
