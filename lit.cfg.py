17cc1dd1ddee16bd5819bcd6cf90a33493190e15
Allen Guo <guosonglin001@gmail.com>
2025-11-29 18:19:36 +0800

    wip

--- lit.cfg.py
+++ lit.cfg.py
@@ -12,11 +12,14 @@ config.name = "mlir_demos"
 config.test_format = lit.formats.ShTest(execute_external=True)
 config.suffixes = set([".mlir"])
 
-config.test_exec_root = os.path.join(tempfile.gettempdir(), "lit")
+config.test_exec_root = os.path.join(
+    tempfile.gettempdir(),
+    f"lit-{os.getenv('USER', '')}",
+)
 # config.test_exec_root = os.path.join("_demos", "lit") # debug
 
 # `FileCheck` or `filecheck` from https://github.com/AntonLydike/filecheck
 filecheck_executable = "FileCheck"
 if not shutil.which(filecheck_executable):
     filecheck_executable = "filecheck"
-    config.substitutions.append(('FileCheck', filecheck_executable))
+    config.substitutions.append(("FileCheck", filecheck_executable))
